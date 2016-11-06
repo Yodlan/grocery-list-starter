f6715f273b28ca4b8784e41d6156f2f5b1239892
Rey <yodlan@yahoo.com>
2016-11-06 13:37:53 -0600

    Grocery list starter

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+// Create your app
+const path = require('path');
+
 const express = require('express');
+const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
+// Initialize our app
 const app = express();
 
-app.user(bodyParser.json());
-app.user(bodyParser.urlencoded({ extended: false }));
+// Set our views directory
+app.use(express.static('css'));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const groceries = require('./router/groceries')
+
+app.use('/groceries', groceries);
+
+// Set up our server
 const port = 3000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Server listening on: ${port}`));
